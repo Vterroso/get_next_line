3b5b5bc4cb7b54d006590531dc5772977657e00b
Victor Terroso <v.terroso87@gmail.com>
2023-04-11 16:00:42 +0200

    update functions in .h

--- get_next_line.c
+++ get_next_line.c
@@ -6,10 +6,15 @@
 /*   By: vterroso <vterroso@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/11 12:14:30 by vterroso          #+#    #+#             */
-/*   Updated: 2023/04/11 12:51:04 by vterroso         ###   ########.fr       */
+/*   Updated: 2023/04/11 15:59:47 by vterroso         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 
 
+char *get_next_line(int fd)
+{
+    char    *buf;
+    
+}
